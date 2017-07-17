00b3c9ba411df877634757bf85b33dac57e8b0b2
Eliza Khachatryan <elijbet@gmail.com>
2017-07-17 16:03:47 -0700

    edit and update functionality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,7 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
+
 	def index
 		@portfolio_items = Portfolio.all
 	end
@@ -13,15 +16,35 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
+        #format.json { render :show, status: :created, location: @portfolios_path }
       else
         format.html { render :new }
-        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+        #format.json { render json: @portfolio.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  def edit
+  end
+
+	 def update
+	  respond_to do |format|
+	    if @portfolio_item.update(portfolio_params)
+	      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+	      #format.json { render :show, status: :ok, location: @portfolios_path }
+	    else
+	      format.html { render :edit }
+	      #format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+	    end
+	  end
+	end
+
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
   	params.require(:portfolio).permit(:title, :subtitle, :body)
