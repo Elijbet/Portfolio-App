c9fb7df2aeb0b5d8b17637bc2724753fa7475fff
Eliza Khachatryan <elijbet@gmail.com>
2017-07-23 12:59:38 -0700

    applied friendly-id feature

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -64,7 +64,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id]) #pull the slug, map it to the id, then find it
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
